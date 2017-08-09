ae473d59f31736a2937586d80d88643c144cdf98
Oleksandr <yaroshenko.aleksandr8@gmail.com>
2017-08-09 15:02:15 +0300

    addded saved searches table

--- app/js/animationController.js
+++ app/js/animationController.js
@@ -1,9 +1,9 @@
 
 var modal = document.getElementById('firstLaunchModal');
 
-var openModalBtn = document.getElementById("openModalBtn");
+var openModalBtn = document.getElementById("openStartModalBtn");
 
-var closeModalBtn = document.getElementById("closeModalBtn");
+var closeModalBtn = document.getElementById("closeStartModalBtn");
 
 closeModalBtn.onclick = function() {
 	modal.style.display = "none";
@@ -12,3 +12,19 @@ closeModalBtn.onclick = function() {
 openModalBtn.onclick = function() {
 	modal.style.display = "block";
 }
+
+$(document).ready(function(){
+     
+     $("body").on("click","#deleteAllSearchesModalBtn",function(){
+             
+          //$("#deleteAllSearchesModal").modal("show");
+       
+          //appending modal background inside the blue div
+          $('.modal-backdrop').appendTo('.table-container');   
+     
+          //remove the padding right and modal-open class from the body tag which bootstrap adds when a modal is shown
+          $('body').removeClass("modal-open")
+          $('body').css("padding-right","");     
+      });
+ 
+});
--- app/js/mainController.js
+++ app/js/mainController.js
@@ -21,4 +21,69 @@ var app = angular.module("SimCast", [])
 		$("#advanced-filters").fadeOut('400', function() {
 		});
 	}
+	$scope.savedSearches = [
+	{
+       name: 'name1 Hey, welcome to SimCast 1.0. Let us know if you have any questions!',
+       upc: '123456123456',
+       company: 'company name',
+       date: "08.08",
+       checked: false
+	},
+	{
+       name: 'a',
+       upc: '12345',
+       company: 'company name',
+       date: "08.08",
+       checked: false
+	},
+	{
+       name: 'name3',
+       upc: '12345',
+       company: 'company name',
+       date: "08.08",
+       checked: false
+	},
+	{
+       name: 'c',
+       upc: '12345',
+       company: 'company name',
+       date: "09.08",
+       checked: false
+	},
+	{
+       name: 'name5',
+       upc: '12345',
+       company: '1company name',
+       date: "08.08",
+       checked: false
+	},
+	{
+       name: 'xvb',
+       upc: '12345',
+       company: 'company name1',
+       date: "08.08",
+       checked: false
+	},
+	{
+       name: 'name7',
+       upc: '12345',
+       company: 'company name',
+       date: "01.08",
+       checked: false
+	},
+	{
+       name: 'name8',
+       upc: '12345',
+       company: 'company name',
+       date: "08.08",
+       checked: false
+	},
+	{
+       name: 'name9',
+       upc: '12345',
+       company: 'company name',
+       date: "08.08",
+       checked: false
+	},
+	];
 });
