2511585c6a53d70aad06544408a15f1ec0f10627
Oleksandr <yaroshenko.aleksandr8@gmail.com>
2017-08-18 23:26:48 +0300

    added fix for heroku #6

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var app = express()
+var http = require('http');
+var fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
+http.createServer(function(req, res){
+    fs.readFile('app/index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8000, function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+}));
