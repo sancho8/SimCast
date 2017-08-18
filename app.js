c4ffe066478c42af586ad8d33b6b00e48a1d9d7f
Oleksandr <yaroshenko.aleksandr8@gmail.com>
2017-08-18 23:54:38 +0300

    added fix for heroku #11

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-var express = require('express')
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    path = require('path');
+
+app.use('/app', express.static('app'));
+app.use('/', function (req, res) {
+    res.sendfile(path.join(__dirname + '/index.html'));
+});
+app.listen(port);
+
+console.log('Server started at http://localhost:%s/', port);
+
+/*var express = require('express')
 var app = express()
 var http = require('http');
 var fs = require('fs');
@@ -9,4 +22,4 @@ http.createServer(function(req, res){
         res.write(data);
         res.end();
     });
-}).listen(process.env.PORT || 5000);
+}).listen(process.env.PORT || 5000);*/
