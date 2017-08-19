42e98e88d7b4f2f4490f09356e78608195c46f9e
Oleksandr <yaroshenko.aleksandr8@gmail.com>
2017-08-19 21:31:29 +0300

    upload working on local machine code

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'app')));
 app.use("/", express.static(__dirname));
-app.use("/img", express.static(__dirname + '/img'));
-app.use("/js", express.static(__dirname + '/js'));
+app.use("/", express.static(__dirname + '/img'));
+app.use("/", express.static(__dirname + '/js'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'index.html'));
+  res.sendFile(path.join(__dirname + '/app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
